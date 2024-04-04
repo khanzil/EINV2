62f05cf210cedcfab46b83ccc1a3264fb0e9396f
Lương Tiến Khanh <128344602+khanzil@users.noreply.github.com>
2024-04-04 14:40:26 +0700

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+source activate ein
 set -e
 
 CONFIG_FILE='./configs/ein_seld/seld.yaml'
 
-python seld/main.py -c $CONFIG_FILE train --seed=$(shuf -i 0-10000 -n 1) --num_workers=8
+python seld/main.py -c $CONFIG_FILE train --seed=$(shuf -i 0-10000 -n 1) --num_workers=4
 
 
 
