00bccac56b0de2ba7aef03d221c28882bd5d6efb
Lương Tiến Khanh <128344602+khanzil@users.noreply.github.com>
2024-04-04 16:31:42 +0700

    Update training.py

--- seld/methods/ein_seld/training.py
+++ seld/methods/ein_seld/training.py
@@ -60,11 +60,11 @@ class Trainer(BaseTrainer):
         """ Perform a train step
 
         """
-        batch_x = batch_sample['waveform']
+        batch_x = batch_sample.batch_out_dict['waveform']
         batch_target = {
-            'ov': batch_sample['ov'],
-            'sed': batch_sample['sed_label'],
-            'doa': batch_sample['doa_label']
+            'ov': batch_sample.batch_out_dict['ov'],
+            'sed': batch_sample.batch_out_dict['sed_label'],
+            'doa': batch_sample.batch_out_dict['doa_label']
         }
         if self.cuda:
             batch_x = batch_x.cuda(non_blocking=True)
@@ -108,8 +108,8 @@ class Trainer(BaseTrainer):
 
                 batch_x = batch_sample.batch_out_dict['waveform']
                 batch_target = {
-                    'sed': batch_sample['sed_label'],
-                    'doa': batch_sample['doa_label']
+                    'sed': batch_sample.batch_out_dict['sed_label'],
+                    'doa': batch_sample.batch_out_dict['doa_label']
                 }
 
                 if self.cuda:
